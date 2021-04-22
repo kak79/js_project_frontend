3f12e40074f355500a2a80ccd1fcf1c4b15323b4
Kimberly Kohel-Hayes <kohelka@gmail.com>
2021-04-22 11:00:57 -0500

    built instance methods

--- scripts/ingredient.js
+++ scripts/ingredient.js
@@ -7,4 +7,21 @@ class Ingredient{
 
   //instance method that renders the object to the DOM
 
+  renderRecipe() {
+    let ingredientDiv = document.getElementById("ingredients-container")
+
+    ingredientDiv.innerHTML +=
+
+    `
+    <br>
+    <ul>
+    <h4>Ingredient Name: ${this.name}</h4>
+    <li>Ingredient Measurement: ${this.measurement}</li>
+    </ul>
+    <br>
+    &nbsp;<button class="delete-button" data-id="${this.id}">Delete Recipe</button>
+    <br>
+    `
+  }
 }
+
