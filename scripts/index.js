a9e8ffade02c04e92225c591081a7b4f0b2e9e92
Kimberly Kohel-Hayes <kohelka@gmail.com>
2021-05-01 13:51:12 -0500

    removed reset functions because found another way

--- scripts/index.js
+++ scripts/index.js
@@ -4,29 +4,9 @@ document.addEventListener("DOMContentLoaded" , () => {
   
   Recipe.fetchRecipes()
 
-  resetFunction1()
-
-  resetFunction2()
-
-  resetFunction3()
-
 })
 
 const BASE_URL = "http://127.0.0.1:3000"
-
-
-function resetFunction1() {
-  // debugger
-  // document.getElementById("new-recipe-form").reset();
-}
-
-function resetFunction2() {
-  // document.getElementById("recipe-search-div").reset();
-}
-
-function resetFunction3() {
-  // document.getElementById("add-ingredient-form").reset();
-}
 
 
 //UPDATE -- edit a recipe -- stretch goal
